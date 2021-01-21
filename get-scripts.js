09ddafde3978263cb559fba905c7ab5ffdefc724
Steve Emerson <sbemerson@outlook.com>
2021-01-21 04:37:14 -0500

    completed module GET Journal Display by Title

--- get-scripts.js
+++ get-scripts.js
@@ -85,7 +85,7 @@ function displayMine() {
 
     }
 
-  })
+  });
 }
   
   
@@ -93,13 +93,148 @@ function displayMine() {
    *** DISPLAY ALL ***
   ************************** */
   function displayAll() {
-   console.log('displayAll Function Called')
-  }
+   //console.log('displayAll Function Called')
+  fetch('http://localhost:3000/journal/', {
+    method: 'GET',
+    headers: new Headers({
+        'Content-Type' : 'application.json',
+    }) 
+  })
+  .then(
+    function(response) {
+      return response.json()
+  })
+  .catch(
+    function(error) {
+      console.error('Error:', error)
+  })
+  .then(function(response){
+    console.log(response);
+    let display = document.getElementById('journals');
+
+    //WHY DOES = WORK HERE AND NOT < ?
+    for(i = 0; i = display.childNodes.length; i++){
+      display.removeChild(display.firstChild);
+    }
+
+    if(response.length === 0){
+      let display = document.getElementById('journals');
+      let header = document.createElement('h5');
   
+      display.appendChild(header);
+      header.textContent = "No posts found";
+      header.setAttribute("class", "noPosts");
+    }else{
+
+      for(i = 0; i < response.length; i++){
+
+        let display = document.getElementById('journals');
+        let card = document.createElement('div');
+        let body = document.createElement('div');
+        let header = document.createElement('h5');
+        let subtitle = document.createElement('h6');
+        let para = document.createElement('p');
+
+        let current = response[i];
+        let title = current.title;
+        let date = current.date;
+        let entry = current.entry;
+
+        display.appendChild(card);
+        card.appendChild(body);
+        body.appendChild(header);
+        body.appendChild(subtitle);
+        body.appendChild(para);
+
+        card.setAttribute('id', current.id);
+        card.setAttribute('class', 'card');
+        body.setAttribute('class', 'card-body');
+        header.setAttribute('class', 'card-title');
+        subtitle.setAttribute('class', 'card-subtitle mb-2 text-muted');
+        para.setAttribute('class', 'card-text');
+
+        header.textContent = title;
+        subtitle.textContent = date;
+        para.textContent = entry;
+
+      }
+    }
+    
+  });
   
+}
   /* *************************
    *** DISPLAY BY TITLE ***
   ************************** */
   function displayByTitle() {
-   console.log('displayByTitle Function Called')
+   //console.log('displayByTitle Function Called')
+    let searchTitle = document.getElementById('searchBar').value;
+    fetch(`http://localhost:3000/journal/${searchTitle}`, {
+      method: 'GET',
+      headers: new Headers({
+          'Content-Type' : 'application.json',
+      }) 
+    })
+    .then(
+      function(response) {
+        return response.json()
+    })
+    .catch(
+      function(error) {
+        console.error('Error:', error)
+    })
+    .then(function(response){
+      console.log(response);
+      let display = document.getElementById('journals');
+  
+      //WHY DOES = WORK HERE AND NOT < ?
+      for(i = 0; i = display.childNodes.length; i++){
+        display.removeChild(display.firstChild);
+      }
+  
+      if(response.length === 0){
+        let display = document.getElementById('journals');
+        let header = document.createElement('h5');
+    
+        display.appendChild(header);
+        header.textContent = "No posts found";
+        header.setAttribute("class", "noPosts");
+      }else{
+  
+        for(i = 0; i < response.length; i++){
+  
+          let display = document.getElementById('journals');
+          let card = document.createElement('div');
+          let body = document.createElement('div');
+          let header = document.createElement('h5');
+          let subtitle = document.createElement('h6');
+          let para = document.createElement('p');
+  
+          let current = response[i];
+          let title = current.title;
+          let date = current.date;
+          let entry = current.entry;
+  
+          display.appendChild(card);
+          card.appendChild(body);
+          body.appendChild(header);
+          body.appendChild(subtitle);
+          body.appendChild(para);
+  
+          card.setAttribute('id', current.id);
+          card.setAttribute('class', 'card');
+          body.setAttribute('class', 'card-body');
+          header.setAttribute('class', 'card-title');
+          subtitle.setAttribute('class', 'card-subtitle mb-2 text-muted');
+          para.setAttribute('class', 'card-text');
+  
+          header.textContent = title;
+          subtitle.textContent = date;
+          para.textContent = entry;
+  
+        }
+      }
+      
+    });
+
   }
